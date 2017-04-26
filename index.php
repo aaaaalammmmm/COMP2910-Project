2d3cec56cdd934190ebed9db1634c4c50fcf2d67
Yashar Nesvaderani <yasharnesvaderani@yashars-air.wifi.bcit.ca>
2017-04-26 11:02:42 -0700

    updated index

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <?php include 'header.php'; ?>
-<body class="container bg-primary">
-
-  <h1 class="text-center">Title Place holder</h1>
-
-  <div class="text-center">SearchBar Placeholder</div>
-  <form>
-    <input type="text" size="30" placeholder="Search Foods..." onkey="showResult(this.value)">
-    <div id="livesearch"></div>
-  </form>
-
+<body class="bg-primary" >
+  <div class="container">
+    <h1 class="text-center">Title Place holder</h1>
+    <div class="text-center">SearchBar Placeholder</div>
+    <form class="text-center">
+      <input type="text" class="inputBox text-center" size="30" placeholder="Search Foods..." onkey="showResult(this.value)">
+      <div id="livesearch"></div>
+    </form>
+  <div class="text-center">
+    <br/>
+    <img src="" alt="image" />
+  </div>
+  </div>
 </body>
 
 <?php include 'footer.php'; ?>
