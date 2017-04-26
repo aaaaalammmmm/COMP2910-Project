87bf26990b9f7a1912a18f2c7ebad131a53c4736
Sergio 'Josh' Villegas <joshviba_7@hotmail.com>
2017-04-26 09:29:06 -0700

    Placeholder text added

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php include 'header.php'; ?>
-<body>
+<body class="container">
 
+  <h1 class="text-center">Title Place holder</h1>
 
+  <div class="text-center">SearchBar Placeholder</div>
 
 
 </body>
