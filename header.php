0d9abcb7ac5107872203e8d673c4cccdfc0d9bfc
Sergio 'Josh' Villegas <joshviba_7@hotmail.com>
2017-04-26 08:35:24 -0700

    JS Bootstrap Libraries

--- header.php
+++ header.php
@@ -5,5 +5,6 @@
   <meta charset="utf-8">
   <link href="styles/bootstrap.min.css" rel="stylesheet" media="screen">
   <link href="styles/style.css" rel="stylesheet" type="text/css" />
-  <script type="text/javascript" src="js/scripts.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
 </head>
