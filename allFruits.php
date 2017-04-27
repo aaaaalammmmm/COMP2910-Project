f0a8120497ae622b55dda59b003511f5d7c4a69a
llowenstein@my.bcit.ca <llowenstein@my.bcit.ca>
2017-04-26 23:06:40 -0700

    Added new images for the main page that link to all fruit page

--- allFruits.php
+++ allFruits.php
@@ -1,30 +1,30 @@
 <?php include 'header.php'; ?>
 <body class="bg-primary" >
   <div class="container">
-    <h1 class="text-center">Title Place holder</h1>
+    <h1 class="text-center">All Fruits</h1>
+    <a href="index.php">Home</a>
     <div class="text-center">SearchBar Placeholder</div>
     <form class="text-center" onsubmit="return false;">
       <input type="text" class="inputBox text-center" size="30" placeholder="Search Foods..." onkeyup="showResult(this.value)">
     </form>
     <div class="text-center">
       <br/>
-      <img src="" alt="image" />
     </div>
     <div id="livesearch"></div>
     <div class="text-center">
-      <div class="row">
-        <div class="col-sm-4">
-          <a href="allFruits.php">
-            <img src="/Images/FruitMedley.jpg" alt="Fruit Medley"/>
-          </a>
-        </div>
-        <div class="col-sm-4">
-          <img src="/Images/VeggieMedley.jpg" alt="Veggie Medley"/>
-        </div>
-        <div class="col-sm-4">
-          <img src="/Images/BreadMedley.jpg" alt="Bread Medley"/>
-        </div>
-      </div>
+      <a href="apple.php">
+        <img src="" alt="Apple"/>
+      </a>
+    </div>
+    <div class="text-center">
+      <a href="banana.php">
+        <img src="" alt="Banana"/>
+      </a>
+    </div>
+    <div class="text-center">
+      <a href="orange.php">
+        <img src="" alt="Orange"/>
+      </a>
     </div>
   </div>
 </body>
--- banana.php
+++ banana.php
@@ -4,11 +4,11 @@ echo
     <img src=\"\" alt=\"Banana\" />
     <div>
       <h3>Storage</h3>
-      <p>Random place holder text.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae dolor dolor. Morbi dapibus risus euismod tortor vestibulum, eget bibendum nisl ultrices. Integer ornare mattis est, et maximus velit tincidunt a. Integer non dapibus enim. Suspendisse nisl urna, molestie sit amet elit a, accumsan commodo nisi. Morbi lacinia dui ipsum. Sed bibendum nisl vitae tortor convallis dapibus eu nec nunc.</p>
     </div>
     <div>
       <h3>Revival</h3>
-      <p>Random place holder text.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae dolor dolor. Morbi dapibus risus euismod tortor vestibulum, eget bibendum nisl ultrices. Integer ornare mattis est, et maximus velit tincidunt a. Integer non dapibus enim. Suspendisse nisl urna, molestie sit amet elit a, accumsan commodo nisi. Morbi lacinia dui ipsum. Sed bibendum nisl vitae tortor convallis dapibus eu nec nunc.</p>
     </div>
     <div>
       <div class=\"col-xs-4\">
