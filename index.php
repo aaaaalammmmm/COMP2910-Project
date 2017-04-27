2da1b2de77c89402c8bd715c9b26327e3a039259
A L <A L>
2017-04-27 15:29:19 -0700

    php server test in index.php

--- index.php
+++ index.php
@@ -1,4 +1,30 @@
 <?php include 'header.php'; ?>
+<?php 
+//This php block will be to test connections with the Azure SQL Database
+
+//These are the lines of code to connect to our database with the credentials
+$connectionInfo = array("UID" => "comp2910@comp2910", "pwd" => "Isitcl3ar", "Database" => "comp2910", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:comp2910.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+//Error handling, if connection fails.
+if( $conn === false ) {
+     die( print_r( sqlsrv_errors(), true));
+}
+
+//This stores the server info of $conn
+$server_info = sqlsrv_server_info( $conn);
+if( $server_info ) {
+	//Print out the the values of each key
+    foreach( $server_info as $key => $value) {
+       echo $key.": ".$value."<br />";
+    }
+} else {
+      die( print_r( sqlsrv_errors(), true));
+}
+
+sqlsrv_close($conn);
+?>
 <body class="bg-primary" >
   <div class="container">
     <h1 class="text-center">Use-It-Up</h1>
