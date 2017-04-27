ac8c26f5a9958d0d1427961f0eb0c4d22219a3b4
Yashar Nesvaderani <yasharnesvaderani@yashars-air.wifi.bcit.ca>
2017-04-27 15:58:00 -0700

    reversed include order of bootstrap and jquery in header.php

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <link href="styles/bootstrap.min.css" rel="stylesheet" media="screen">
   <link href="styles/style.css" rel="stylesheet" type="text/css" />
-  <script type="text/javascript" src="js/bootstrap.min.js"></script>
   <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
   <script type="text/javascript" src="js/scripts.js"></script>
 </head>
