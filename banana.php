fe7e9004129b6e25886c590886551a3e655b7ebc
Sergio 'Josh' Villegas <joshviba_7@hotmail.com>
2017-04-28 12:46:47 -0700

    Redirect test

--- banana.php
+++ banana.php
@@ -10,13 +10,13 @@
   </div>
   <div>
     <div class="col-xs-4">
-      <a onclick="showResult(green-banana)"><img src="" alt="Green Banana" /></a><div>Green Banana</div>
+      <span onclick="showResult(green-banana)"><img src="" alt="Green Banana" /></span><div>Green Banana</div>
     </div>
     <div class="col-xs-4">
-      <a onclick="showResult(yellow-banana)"><img src="" alt="Yellow/Ripe Banana" /></a><div>Yellow/Ripe Banana</div>
+      <span onclick="showResult(yellow-banana)"><img src="" alt="Yellow/Ripe Banana" /></span><div>Yellow/Ripe Banana</div>
     </div>
     <div class="col-xs-4">
-      <a onclick="showResult(black-banana)"><img src="" alt="Black/Over-ripe Banana" /></a><div>Black/Over-ripe Banana</div>
+      <span onclick="showResult(black-banana)"><img src="" alt="Black/Over-ripe Banana" /></span><div>Black/Over-ripe Banana</div>
     </div>
   </div>
 </div>
