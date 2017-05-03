da7393d119b77dfc20e9189f7635ed4a9a1c2ff1
yoshimitz <yashar93@gmail.com>
2017-05-03 12:30:51 -0700

    homepage hamburger button

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <title>Use-It-Up</title>
-  <meta charset="utf-8">
+  <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="styles/bootstrap.min.css" rel="stylesheet" media="screen">
   <link href="styles/style.css" rel="stylesheet" type="text/css" />
   <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
--- index.php
+++ index.php
@@ -2,30 +2,51 @@
 <?php
 //This php block will be to test connections with the Azure SQL Database
 
-//These are the lines of code to connect to our database with the credentials
-$connectionInfo = array("UID" => getenv('SQLAZURECONNSTR_UID'), "pwd" => getenv('SQLAZURECONNSTR_pwd'), "Database" => getenv('SQLAZURECONNSTR_database'), "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = getenv('SQLAZURECONNSTR_serverName');
-$conn = sqlsrv_connect($serverName, $connectionInfo);
-
-//Error handling, if connection fails.
-if( $conn === false ) {
-  die( print_r( sqlsrv_errors(), true));
-}
-
-//This stores the server info of $conn
-$server_info = sqlsrv_server_info( $conn);
-if( $server_info ) {
-  //Print out the the values of each key
-  foreach( $server_info as $key => $value) {
-    echo $key.": ".$value."<br />";
-  }
-} else {
-  die( print_r( sqlsrv_errors(), true));
-}
-
-sqlsrv_close($conn);
+// //These are the lines of code to connect to our database with the credentials
+// $connectionInfo = array("UID" => getenv('SQLAZURECONNSTR_UID'), "pwd" => getenv('SQLAZURECONNSTR_pwd'), "Database" => getenv('SQLAZURECONNSTR_database'), "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+// $serverName = getenv('SQLAZURECONNSTR_serverName');
+// $conn = sqlsrv_connect($serverName, $connectionInfo);
+//
+// //Error handling, if connection fails.
+// if( $conn === false ) {
+//   die( print_r( sqlsrv_errors(), true));
+// }
+//
+// //This stores the server info of $conn
+// $server_info = sqlsrv_server_info( $conn);
+// if( $server_info ) {
+//   //Print out the the values of each key
+//   foreach( $server_info as $key => $value) {
+//     echo $key.": ".$value."<br />";
+//   }
+// } else {
+//   die( print_r( sqlsrv_errors(), true));
+// }
+//
+// sqlsrv_close($conn);
 ?>
 <body class="bg-primary" >
+  <div class="container">
+  <nav class="navbar navbar-default navbar-static-top" role="navigation">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed pull-left" 
+        data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle Navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+      </div>
+
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li><a href="index.php">Home</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</div>
   <div class="container col-xs-12">
     <div class="col-xs-10 col-xs-offset-1" >
       <h1 class="text-center">Use-It-Up</h1>
