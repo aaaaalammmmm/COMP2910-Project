9fc7c11518d0a8d6647dff5f2a354d7414fa22c0
Sergio 'Josh' Villegas <joshviba_7@hotmail.com>
2017-05-05 13:21:58 -0700

    CSS commenting. Removed footer.

--- footer.php
+++ footer.php
@@ -1,14 +1,3 @@
-<footer class="navbar-fixed-bottom">
-  <div class="container padding-sm text-center">
-    <div class="col-xs-4 footer">
-      Facebook
-    </div>
-    <div class="col-xs-4 footer">
-      Email
-    </div>
-    <div class="col-xs-4 footer">
-      Info
-    </div>
-  </div>
+<footer>
 </footer>
 </html>
