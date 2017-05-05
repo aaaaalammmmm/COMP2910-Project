19ca5c5e68fb01fd1c1b194df677f17baf5b6558
Sergio 'Josh' Villegas <joshviba_7@hotmail.com>
2017-05-05 14:13:27 -0700

    Banana Hamburger menu update. Indentation fixes: foodHeader.php, banana.php, styles.css, header.php

--- banana.php
+++ banana.php
@@ -5,71 +5,70 @@
   <img src="images/Banana.png" alt="Banana" />
   <div>
     <h3>Storage</h3>
-	<div id="bananaStorage"></div>
-	<h3>Recipes</h3>
-	<div id="bananaRecipes"></div>
+    <div id="bananaStorage"></div>
+    <h3>Recipes</h3>
+    <div id="bananaRecipes"></div>
   </div>
   <!-- Redirection for further info on food state -->
   <div class="row">
     <div class="col-xs-4 pull-left">
-        <img src="images/Banana.png" alt="Underripe Banana" onclick="bananaInformation('underripe')"/>
-        <div>Underripe</div>
+      <img src="images/Banana.png" alt="Underripe Banana" onclick="bananaInformation('underripe')"/>
+      <div>Underripe</div>
     </div>
     <div class="col-xs-4">
-        <img src="images/Banana.png" alt="Ripe Banana" onclick="bananaInformation('ripe')"/>
-        <div>Ripe</div>
+      <img src="images/Banana.png" alt="Ripe Banana" onclick="bananaInformation('ripe')"/>
+      <div>Ripe</div>
     </div>
     <div class="col-xs-4 pull-right">
-		<img src="images/Banana.png" alt="Overripe Banana" onclick="bananaInformation('overripe')"/>
-        <div>Overripe</div>
+      <img src="images/Banana.png" alt="Overripe Banana" onclick="bananaInformation('overripe')"/>
+      <div>Overripe</div>
     </div>
   </div>
 </div>
 <br />
 <script>
-	//This stores a pointer to all info about bananas
-	var bananaInfo = rootRef.child("fruit/banana");
-	//This creates a pointer to the banana storage div
-	var bananaStorageDiv  = document.getElementById("bananaStorage");	
-	//Test variable
-	console.log(bananaStorageDiv);
-	//This creates a pointer to the banana recipes div
-	var bananaRecipesDiv  = document.getElementById("bananaRecipes");
-		//Test variable
-	console.log(bananaRecipesDiv);
-	
-	//This function will pull the string containing information about storage
-	//and then assigns it to the storage div
-	function bananaInformation(state) {
-		//This stores the location of the banana storage
-		var bananaStateInfo;
-		//This stores the string from the database
-		var bananaStorageText;
-		//This stores the string from the database
-		var bananaRecipesText;
-		
-		//Go to the child node containing the state for the banana
-		bananaStateInfo = bananaInfo.child(state);
-		//Create a snapshot of the banana state node
-		bananaStateInfo.once("value")
-		.then(function(snapshot) {
-			//store the contents of the node as a string variable
-			bananaStorageText = snapshot.child("storage").val();
-			//store the contents of the node as a string variable
-			bananaRecipesText = snapshot.child("recipes").val();
-			//check to see if it is stored in the variable
-			console.log(bananaStorageText);
-			//check to see if it is stored in the variable
-			console.log(bananaRecipesText);
-			//Assign the string as inner html to the storage div		
-			bananaStorageDiv.innerHTML = bananaStorageText;
-			//Assign the string as inner html to the recipes div		
-			bananaRecipesDiv.innerHTML = bananaRecipesText;
-		});
-	}
-	
-	onload = bananaInformation("ripe");
+//This stores a pointer to all info about bananas
+var bananaInfo = rootRef.child("fruit/banana");
+//This creates a pointer to the banana storage div
+var bananaStorageDiv  = document.getElementById("bananaStorage");
+//Test variable
+console.log(bananaStorageDiv);
+//This creates a pointer to the banana recipes div
+var bananaRecipesDiv  = document.getElementById("bananaRecipes");
+//Test variable
+console.log(bananaRecipesDiv);
+
+//This function will pull the string containing information about storage
+//and then assigns it to the storage div
+function bananaInformation(state) {
+  //This stores the location of the banana storage
+  var bananaStateInfo;
+  //This stores the string from the database
+  var bananaStorageText;
+  //This stores the string from the database
+  var bananaRecipesText;
+
+  //Go to the child node containing the state for the banana
+  bananaStateInfo = bananaInfo.child(state);
+  //Create a snapshot of the banana state node
+  bananaStateInfo.once("value")
+  .then(function(snapshot) {
+    //store the contents of the node as a string variable
+    bananaStorageText = snapshot.child("storage").val();
+    //store the contents of the node as a string variable
+    bananaRecipesText = snapshot.child("recipes").val();
+    //check to see if it is stored in the variable
+    console.log(bananaStorageText);
+    //check to see if it is stored in the variable
+    console.log(bananaRecipesText);
+    //Assign the string as inner html to the storage div
+    bananaStorageDiv.innerHTML = bananaStorageText;
+    //Assign the string as inner html to the recipes div
+    bananaRecipesDiv.innerHTML = bananaRecipesText;
+  });
+}
+
+onload = bananaInformation("ripe");
 
 </script>
 <?php include 'footer.php'; ?>
-
--- header.php
+++ header.php
@@ -12,27 +12,27 @@
   <script src="https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js"></script>
   <script src="https://www.gstatic.com/firebasejs/3.9.0/firebase-database.js"></script>
   <script>
-	  //Configurations for the firebase initialization
-	  var config = {
-		apiKey: "AIzaSyCS3TPF_o_6n52S9wevo7sz6k3h3V4FMS8",
-		authDomain: "comp2910-b5e23.firebaseapp.com",
-		databaseURL: "https://comp2910-b5e23.firebaseio.com",
-		projectId: "comp2910-b5e23",
-		storageBucket: "comp2910-b5e23.appspot.com",
-		messagingSenderId: "230859715437"
-	  };
-	  //Initialize firebase application
-	  firebase.initializeApp(config);
+  //Configurations for the firebase initialization
+  var config = {
+    apiKey: "AIzaSyCS3TPF_o_6n52S9wevo7sz6k3h3V4FMS8",
+    authDomain: "comp2910-b5e23.firebaseapp.com",
+    databaseURL: "https://comp2910-b5e23.firebaseio.com",
+    projectId: "comp2910-b5e23",
+    storageBucket: "comp2910-b5e23.appspot.com",
+    messagingSenderId: "230859715437"
+  };
+  //Initialize firebase application
+  firebase.initializeApp(config);
 
-	  //Getting an instance of the database
-	  var database = firebase.database();
-	  //Enable logging
-	  firebase.database.enableLogging(true);
+  //Getting an instance of the database
+  var database = firebase.database();
+  //Enable logging
+  firebase.database.enableLogging(true);
 
-	  //Check and print the root key of the database on browser console
-	  console.log(database.ref().key);
-	  
-	  //Set to rootRef to the food node
-	  var rootRef = database.ref("food");
+  //Check and print the root key of the database on browser console
+  console.log(database.ref().key);
+
+  //Set to rootRef to the food node
+  var rootRef = database.ref("food");
   </script>
 </head>
