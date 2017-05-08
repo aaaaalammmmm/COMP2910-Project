72ba8021fed18c3b8c0ab39598cba4b1f74dc2f9
Sergio 'Josh' Villegas <joshviba_7@hotmail.com>
2017-05-08 13:14:27 -0700

    Fixed mino bug: banana.php redirects to index.html

--- food-header.php
+++ food-header.php
@@ -60,7 +60,7 @@
           </div>
           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-              <li><a class="text-black" href="index.html">Home</a></li>
+              <li><a class="text-black" href="index.php">Home</a></li>
               <li><a class="text-black" href="about.php">About Us</a></li>
               <li><a class="text-black" href="contact.php">Contact Us</a></li>
               <li><a class="text-black" href="info.php">Info</a></li>
