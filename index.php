91de5dd995dbf8f64ba5b0cb69aa2abbc72c875a
yoshimitz <yashar93@gmail.com>
2017-05-10 13:02:59 -0700

    fixed hamburger button cutting things off

--- index.php
+++ index.php
@@ -2,14 +2,7 @@
 <body class="bg-primary">
 <div class="container">
     <div>
-        <div>
-            <a href="index.php">
-                <img class="center-block" src="Images/UseItUpBanner v2.0.png"/>
-            </a>
-        </div>
-    </div>
-    <div class="row top-button">
-        <div class="col-xs-12">
+        <div class="container-fluid">
             <nav class="navbar navbar-default" role="navigation">
                 <div>
                     <div class="navbar-header">
@@ -28,8 +21,14 @@
                             <li><a class="text-black" href="info.php">Info</a></li>
                         </ul>
                     </div>
+
                 </div>
             </nav>
+        </div>
+        <div>
+            <a href="index.php">
+                <img class="center-block" src="Images/UseItUpBanner v2.0.png"/>
+            </a>
         </div>
     </div>
     <div>
