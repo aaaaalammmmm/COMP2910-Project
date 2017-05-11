1927cd9263ff7aea4111fbeb8eac1afc9e2e68fa
yoshimitz <yashar93@gmail.com>
2017-05-10 21:16:19 -0700

    added desktop only homepage images

--- food.php
+++ food.php
@@ -9,95 +9,95 @@ $type = $_GET['t'];
 
 ?>
 <div class="container">
-<!-- Back button -->
-<div id="back-button" class="col-xs-12 padding-sm" style="z-index:1">
-  <span  class="glyphicon glyphicon-menu-left" onclick="goBack('<?php echo $type ?>')"/>
-</div>
-<br>
-<!-- Information on a food item -->
-<div class="text-center col-xs-12">
-  <img src=<?php echo "images/".$food.".png";?> class="single-food-imagesize" alt=<?php echo $food; ?> />
-  <div>
-    <h3>Storage</h3>
-    <div id="storage"></div>
-    <h3>Recipes</h3>
-    <div id="recipes"></div>
+  <!-- Back button -->
+  <div id="back-button" class="col-xs-12 padding-sm" style="z-index:1">
+    <span  class="glyphicon glyphicon-menu-left" onclick="goBack('<?php echo $type ?>')"/>
   </div>
-  <!-- Redirection for further info on food state -->
-  <div class="row">
-    <div class="col-xs-4 pull-left">
-      <?php if($food === "bread") {
-        echo "<img src='" . "images/" . $food . ".png'" . "class='single-food-imagesize' alt='Fresh " . $food . "' onclick='" . "foodInformation(\"fresh\")'/>";
-        echo "<div>Fresh</div>";
-      } else if($type === "grain"){
-        echo "<img src='" . "images/" . $food . "-R.png'" . "class='single-food-imagesize' alt='Raw " . $food . "' onclick='" . "foodInformation(\"raw\")'/>";
-        echo "<div>Raw</div>";
-      } else {
-        echo "<img src='" . "images/" . $food . "-UR.png'" . "class='single-food-imagesize' alt='Underripe " . $food . "' onclick='" . "foodInformation(\"underripe\")'/>";
-        echo "<div>Underripe</div>";
-      }?>
+  <br>
+  <!-- Information on a food item -->
+  <div class="text-center col-xs-12">
+    <img src=<?php echo "images/".$food.".png";?> class="single-food-imagesize" alt=<?php echo $food; ?> />
+    <div>
+      <h3>Storage</h3>
+      <div id="storage"></div>
+      <h3>Recipes</h3>
+      <div id="recipes"></div>
     </div>
-    <div class="col-xs-4">
-      <?php if($type === "grain") {
-        echo "<div></div>";
-      }else{
-        echo "<img src='" . "images/" . $food . ".png'" . "class='single-food-imagesize' alt='Ripe " . $food . "' onclick='" . "foodInformation(\"ripe\")'/>";
-        echo "<div>Ripe</div>";
-      }?>
-	</div>
-    <div class="col-xs-4 pull-right">
-      <?php if($food === "bread") {
-        echo "<img src='" . "images/" . $food . ".png'" . " class='single-food-imagesize' alt='Stale " . $food . "' onclick='" . "foodInformation(\"stale\")'/>";
-        echo "<div>Stale</div>";
-      } else if($type === "grain"){
-        echo "<img src='" . "images/" . $food . "-C.png'" . " class='single-food-imagesize' alt='Cooked " . $food . "' onclick='" . "foodInformation(\"cooked\")'/>";
-        echo "<div>Cooked</div>";
-      }else{
-        echo "<img src='" . "images/" . $food . "-OR.png'" . " class='single-food-imagesize' alt='Overripe " . $food . "' onclick='" . "foodInformation(\"overripe\")'/>";
-        echo "<div>Overripe</div>";
-      }?>
+    <!-- Redirection for further info on food state -->
+    <div class="row">
+      <div class="col-xs-4 pull-left">
+        <?php if($food === "bread") {
+          echo "<img src='" . "images/" . $food . ".png'" . "class='single-food-imagesize' alt='Fresh " . $food . "' onclick='" . "foodInformation(\"fresh\")'/>";
+          echo "<div>Fresh</div>";
+        } else if($type === "grain"){
+          echo "<img src='" . "images/" . $food . "-R.png'" . "class='single-food-imagesize' alt='Raw " . $food . "' onclick='" . "foodInformation(\"raw\")'/>";
+          echo "<div>Raw</div>";
+        } else {
+          echo "<img src='" . "images/" . $food . "-UR.png'" . "class='single-food-imagesize' alt='Underripe " . $food . "' onclick='" . "foodInformation(\"underripe\")'/>";
+          echo "<div>Underripe</div>";
+        }?>
+      </div>
+      <div class="col-xs-4">
+        <?php if($type === "grain") {
+          echo "<div></div>";
+        }else{
+          echo "<img src='" . "images/" . $food . ".png'" . "class='single-food-imagesize' alt='Ripe " . $food . "' onclick='" . "foodInformation(\"ripe\")'/>";
+          echo "<div>Ripe</div>";
+        }?>
+      </div>
+      <div class="col-xs-4 pull-right">
+        <?php if($food === "bread") {
+          echo "<img src='" . "images/" . $food . ".png'" . " class='single-food-imagesize' alt='Stale " . $food . "' onclick='" . "foodInformation(\"stale\")'/>";
+          echo "<div>Stale</div>";
+        } else if($type === "grain"){
+          echo "<img src='" . "images/" . $food . "-C.png'" . " class='single-food-imagesize' alt='Cooked " . $food . "' onclick='" . "foodInformation(\"cooked\")'/>";
+          echo "<div>Cooked</div>";
+        }else{
+          echo "<img src='" . "images/" . $food . "-OR.png'" . " class='single-food-imagesize' alt='Overripe " . $food . "' onclick='" . "foodInformation(\"overripe\")'/>";
+          echo "<div>Overripe</div>";
+        }?>
+      </div>
     </div>
   </div>
-</div>
-<br />
+  <br />
 </div>
 <script>
-var food = "<?php echo $food; ?>";
-//This stores a pointer to all info about bananas
-var foodInfo = rootRef.child("fruit/"+ food);
-//This creates a pointer to the food item storage div
-var storageDiv  = document.getElementById("storage");
-//This creates a pointer to the food item recipes div
-var recipesDiv  = document.getElementById("recipes");
+  var food = "<?php echo $food; ?>";
+  //This stores a pointer to all info about bananas
+  var foodInfo = rootRef.child("fruit/"+ food);
+  //This creates a pointer to the food item storage div
+  var storageDiv  = document.getElementById("storage");
+  //This creates a pointer to the food item recipes div
+  var recipesDiv  = document.getElementById("recipes");
 
-//This function will pull the string containing information about storage
-//and then assigns it to the storage div
-function foodInformation(state) {
-  //This stores the location of the banana storage
-  var stateInfo;
-  //This stores the string from the database
-  var storageText;
-  //This stores the string from the database
-  var recipesText;
+  //This function will pull the string containing information about storage
+  //and then assigns it to the storage div
+  function foodInformation(state) {
+    //This stores the location of the banana storage
+    var stateInfo;
+    //This stores the string from the database
+    var storageText;
+    //This stores the string from the database
+    var recipesText;
 
-  //Go to the child node containing the state for the banana
-  stateInfo = foodInfo.child(state);
-  //Create a snapshot of the banana state node
-  stateInfo.once("value")
-  .then(function(snapshot) {
-    //store the contents of the node as a string variable
-    storageText = snapshot.child("storage").val();
-    //store the contents of the node as a string variable
-    recipesText = snapshot.child("recipes").val();
-    //Assign the string as inner html to the storage div
-    storageDiv.innerHTML = storageText;
-    //Assign the string as inner html to the recipes div
-    recipesDiv.innerHTML = recipesText;
-  });
-}
+    //Go to the child node containing the state for the banana
+    stateInfo = foodInfo.child(state);
+    //Create a snapshot of the banana state node
+    stateInfo.once("value")
+      .then(function(snapshot) {
+        //store the contents of the node as a string variable
+        storageText = snapshot.child("storage").val();
+        //store the contents of the node as a string variable
+        recipesText = snapshot.child("recipes").val();
+        //Assign the string as inner html to the storage div
+        storageDiv.innerHTML = storageText;
+        //Assign the string as inner html to the recipes div
+        recipesDiv.innerHTML = recipesText;
+      });
+  }
 
-// Loads ripe food item as default
-onload = foodInformation("ripe");
+  // Loads ripe food item as default
+  onload = foodInformation("ripe");
 
 </script>
 <?php include 'footer.php'; ?>
