17230c1601d3359dc2b0e571fc7e7bc248bbc575
llowenstein@my.bcit.ca <llowenstein@my.bcit.ca>
2017-05-18 22:05:47 -0700

    Centered text and justified it. changed font

--- affiliates.php
+++ affiliates.php
@@ -10,21 +10,21 @@
   <script type="text/javascript" src="js/scripts.js"></script>
 </head>
 <?php include 'header.php';?>
-  <h1 class="text-center">Our Partners</h1>
+  <h4 class="text-center"><b>Our Partners</b></h4>
   <div class="row text-center">
     <div class="col-md-4 padding-md"><img src="Images/PlentyOfThymeLogo.png" class="padding-md affiliate-size" alt="Plenty of Thyme Logo"/>
-      <p><br/>Plenty of Thyme is a web based grocery application that will keep track of the items in your fridge and their expiry dates. You can input a grocery item, its price, and it's given expiry date. The application will provide a statistic of how much food and money you have wasted to help reduce food waste.</p></div>
+      <p class="text-left"><br/>Plenty of Thyme is a web based grocery application that will keep track of the items in your fridge and their expiry dates. You can input a grocery item, its price, and it's given expiry date. The application will provide a statistic of how much food and money you have wasted to help reduce food waste.</p></div>
     <div class="col-md-4 padding-md">
       <a href="http://foodfall.ca">
         <img src="Images/food-fall-logo.png" class="affiliate-size" alt="Food Fall Logo"/>
       </a>
-      <p><br/>Food Fall is a JavaScript based game educating users on the effects of food waste in a fun and interactive environment. See how much produce you can catch without losing, and learn about how to mitigate food waste while having fun.</p>
+      <p class="text-left"><br/>Food Fall is a JavaScript based game educating users on the effects of food waste in a fun and interactive environment. See how much produce you can catch without losing, and learn about how to mitigate food waste while having fun.</p>
     </div>
     <div class="col-md-4 padding-md">
       <a href="https://greendeals.me/">
         <img src="Images/GreenDealsLogo.png" class="affiliate-size" alt ="Green Deals Logo"/>
       </a>
-      <p><br/>Green Deals is a platform that allows grocers to easily sell food products that are expiring soon. Green Deals endeavours to reduce food waste by ensuring that all food carried by grocery stores is sold to consumers by the best before date. This will reduce food waste at the ground level and provide great deals to consumers.
+      <p class="text-left"><br/>Green Deals is a platform that allows grocers to easily sell food products that are expiring soon. Green Deals endeavours to reduce food waste by ensuring that all food carried by grocery stores is sold to consumers by the best before date. This will reduce food waste at the ground level and provide great deals to consumers.
       </p>
     </div>
   </div>
--- info.php
+++ info.php
@@ -2,20 +2,22 @@
 <!--Contains info text-->
 <div class="container">
   <div class="text-center">
-    <h4>Why food waste matters</h4>
+    <h4><b>Why food waste matters</b></h4>
   </div>
   <div>
     <div class="aboutText">
-      <p>47% of wasted food comes from consumers, most of it as avoidable food waste. Avoidable food waste is food that could have been eaten but was thrown away or composted instead. There are three main reasons why food waste is a problem:</p>
+      <p>47% of wasted food comes from consumers, most of it as avoidable food waste.There are three main reasons why food waste is a problem:</p>
       <div class="aboutList">
         <!--Description list for reasons why food waste is important-->
         <dl>
           <dt>Money</dt>
             <dd>Food waste costs Canada $21 Billion a year. Of that, 50% is avoidable waste.
             </dd>
+            <br/>
           <dt>Environment</dt>
-            <dd>Agriculture uses 80% of the world’s water, 40% of the world’s land, and 10% of the world’s energy. Yet, a third of all food is wasted.
+            <dd>Agriculture uses 80% of the world’s water, 40% of the world’s land, and 10% of the world’s energy.
             </dd>
+            <br/>
           <dt>Ethics</dt>
             <dd>It is immoral that so many people struggle to find food every day, yet 222 million tonnes of edible food per year goes to waste in developed countries. Even locally, 1 of 8 Canadian families struggle to put food on the table.
           </dd>
@@ -24,7 +26,7 @@
     </div>
   </div>
   <div class="text-center">
-    <h4>How are we trying to help?</h4>
+    <h4><b>How are we trying to help?</b></h4>
   </div>
   <div class="aboutText">
     <p>Most overripe fruits can still be used in a variety of recipes, but are thrown out because they don’t look good enough. Similarly, wilted produce can often be revived instead of being thrown out.</p>
