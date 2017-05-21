5fdd708a34697f9829413f36a66a17067e7904e0
llowenstein@my.bcit.ca <llowenstein@my.bcit.ca>
2017-05-20 20:07:34 -0700

    css comments, width changes on large devices

--- about.php
+++ about.php
@@ -4,7 +4,7 @@
   <div class="text-center">
     <h4><b>About Us</b></h4>
   </div>
-  <div>
+  <div class="aboutUs">
     <div class="aboutText">
       <p>Our team is comprised of five second year students at the British Columbia Institute of Technology, all currently in the Computer Systems Technology diploma. We are highly dedicated and motivated students, as well as close friends.</p>
       <p>As a team, we recognize that the best way to tackle the food waste problem is by education and awareness. We created Use It Up to give consumers options for overripe foods that does not include the compost bin. Our main inspiration is the banana. In a survey, 41% of canadian citizens said they frequently throw out overripe bananas.</p>
