709a42a925ea198e508a0c1df99ea94f1f1b2d58
A L <A L>
2017-05-24 14:49:46 -0700

    fade out length is shortened, applied on all*.php

--- allGrains.php
+++ allGrains.php
@@ -2,10 +2,24 @@
 // Adds the ajax liveload to the ajax history
 dhtmlHistory.add("allGrains","allGrains");
 </script>
+<script type="text/javascript">
+$(document).ready(function() {
+
+  $("a.transition").click(function(event){
+      event.preventDefault();
+      linkLocation = this.href;
+      $("body").fadeOut(1500, redirectPage);
+  });
+
+  function redirectPage() {
+      window.location = linkLocation;
+  }
+});
+</script>
 <!-- This page loads all the grains  -->
 <div class="row text-center block-center">
   <div class="col-lg-2 col-md-3 col-xs-4">
-    <a href="javascript:pageLoad('bread','grains')">
+    <a href="javascript:pageLoad('bread','grains')" class="transition">
       <figure>
         <img src="images/bread.png" alt="Bread"/>
         <figcaption>Bread</figcaption>
@@ -14,7 +28,7 @@ dhtmlHistory.add("allGrains","allGrains");
     </a>
   </div>
   <div class="col-lg-2 col-md-3 col-xs-4">
-    <a href="javascript:pageLoad('pasta','grains')">
+    <a href="javascript:pageLoad('pasta','grains')" class="transition">
       <figure>
         <img src="images/pasta.png" alt="Pasta"/>
         <figcaption>Pasta</figcaption>
@@ -23,7 +37,7 @@ dhtmlHistory.add("allGrains","allGrains");
     </a>
   </div>
   <div class="col-lg-2 col-md-3 col-xs-4">
-    <a href="javascript:pageLoad('quinoa','grains')">
+    <a href="javascript:pageLoad('quinoa','grains')" class="transition">
       <figure>
         <img src="images/quinoa.png" alt="Quinoa"/>
         <figcaption>Quinoa</figcaption>
@@ -32,7 +46,7 @@ dhtmlHistory.add("allGrains","allGrains");
     </a>
   </div>
   <div class="col-lg-2 col-md-3 col-xs-4">
-    <a href="javascript:pageLoad('rice','grains')">
+    <a href="javascript:pageLoad('rice','grains')" class="transition">
       <figure>
         <img src="images/rice.png" alt="Rice"/>
         <figcaption>Rice</figcaption>
--- allVeggies.php
+++ allVeggies.php
@@ -2,10 +2,24 @@
 // Adds the ajax liveload to the ajax history
 dhtmlHistory.add("allVeggies","allVeggies");
 </script>
+<script type="text/javascript">
+$(document).ready(function() {
+
+  $("a.transition").click(function(event){
+      event.preventDefault();
+      linkLocation = this.href;
+      $("body").fadeOut(1500, redirectPage);
+  });
+
+  function redirectPage() {
+      window.location = linkLocation;
+  }
+});
+</script>
 <!-- This page loads all the veggies  -->
 <div class="row text-center center-block">
   <div class="col-lg-2 col-md-3 col-xs-4">
-    <a href="javascript:pageLoad('broccoli','vegetable')">
+    <a href="javascript:pageLoad('broccoli','vegetable')" class="transition">
         <figure>
             <img src="Images/Broccoli.png" alt="Broccoli"/>
             <figcaption>Broccoli</figcaption>
@@ -13,7 +27,7 @@ dhtmlHistory.add("allVeggies","allVeggies");
     </a>
   </div>
   <div class="col-lg-2 col-md-3 col-xs-4">
-    <a href="javascript:pageLoad('carrot','vegetable')">
+    <a href="javascript:pageLoad('carrot','vegetable')" class="transition">
         <figure>
             <img src="Images/Carrot.png" alt="Carrot"/>
             <figcaption>Carrot</figcaption>
@@ -21,7 +35,7 @@ dhtmlHistory.add("allVeggies","allVeggies");
     </a>
   </div>
   <div class="col-lg-2 col-md-3 col-xs-4">
-    <a href="javascript:pageLoad('celery','vegetable')">
+    <a href="javascript:pageLoad('celery','vegetable')" class="transition">
         <figure>
             <img src="Images/Celery.png" alt="Celery"/>
             <figcaption>Celery</figcaption>
@@ -29,7 +43,7 @@ dhtmlHistory.add("allVeggies","allVeggies");
     </a>
   </div>
   <div class="col-lg-2 col-md-3 col-xs-4">
-    <a href="javascript:pageLoad('lettuce','vegetable')">
+    <a href="javascript:pageLoad('lettuce','vegetable')" class="transition">
         <figure>
             <img src="Images/Lettuce.png" alt="Lettuce"/>
             <figcaption>Lettuce</figcaption>
@@ -37,7 +51,7 @@ dhtmlHistory.add("allVeggies","allVeggies");
     </a>
   </div>
   <div class="col-lg-2 col-md-3 col-xs-4">
-    <a href="javascript:pageLoad('tomato','vegetable')">
+    <a href="javascript:pageLoad('tomato','vegetable')" class="transition">
       <figure>
           <img src="Images/Tomato.png" alt="Tomato"/>
           <figcaption>Tomato</figcaption>
@@ -45,7 +59,7 @@ dhtmlHistory.add("allVeggies","allVeggies");
   </a>
   </div>
     <div class="col-lg-2 col-md-3 col-xs-4">
-    <a href="javascript:pageLoad('potato','vegetable')">
+    <a href="javascript:pageLoad('potato','vegetable')" class="transition">
         <figure>
             <img src="Images/Potato.png" alt="Potato"/>
             <figcaption>Potato</figcaption>
