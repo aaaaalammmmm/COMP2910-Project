8b80a49c9b374885164147f6af51534099406c8c
Sergio 'Josh' Villegas <joshviba_7@hotmail.com>
2017-05-24 13:49:26 -0700

    Favicon linked for the rest of pages

--- general-header.php
+++ general-header.php
@@ -3,7 +3,7 @@
 <head>
   <title>Use-It-Up</title>
   <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" type="image/x-icon" href="http://example.com/favicon.ico" />
+  <link rel="icon" type="image/x-icon" href="images/favicon.ico" />
   <link href="styles/bootstrap.min.css" rel="stylesheet" media="screen">
   <link href="styles/style.css" rel="stylesheet" type="text/css" />
   <style>
