2be942f1d2f0ad25057f1fe9ed389198c903a68a
yoshimitz <yashar93@gmail.com>
2017-05-25 14:39:47 -0700

    changed back and forth buttons to spans

--- food.php
+++ food.php
@@ -10,9 +10,9 @@ $food = $_GET['f'];
 ?>
 <div id="main-content" class="text-center">
   <h3><?php echo ucfirst($food);?></h3>
-  <button class="btn-link glyphicon glyphicon-chevron-left" onclick="prevFood()"></button>
+  <span class="glyphicon glyphicon-chevron-left" onclick="prevFood()"></span>
   <img id="image" src=<?php if($food == "bread"){ echo "images/".$food."-S.png";} else if($type == "grains"){ echo "images/".$food."-R.png"; } else {echo "images/".$food.".png"; }?> class="single-food-imagesize" alt=<?php echo $food; ?> />
-  <button class="btn-link glyphicon glyphicon-chevron-right" onclick="nextFood()"></button>
+  <span class="glyphicon glyphicon-chevron-right" onclick="nextFood()"></span>
   <div>
     <span onclick="" data-toggle="modal" data-target="#share" class="glyphicon glyphicon-share"></span>
     <br/>
